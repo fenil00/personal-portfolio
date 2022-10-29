95d2b14383a0f4b531feb432e38cf7dc4deb4e2f
fenil00 <fenildesai00@gmail.com>
2022-10-29 20:06:11 +0200

    bug fix

--- pages/resume.js
+++ pages/resume.js
@@ -1,4 +1,24 @@
-let bars = document.querySelectorAll(".skillbar-bar");
-for(let bar of bars){
-    bar.style.width = `${String(Math.min(100, Math.max((bar.getAttribute("data-width") / 5.0) * 100.0, 0)))}%`;
-}
+var pgms = document.getElementById("pills-programming-tab");
+var pgsection = document.getElementById("programming");
+
+pgms.addEventListener('click', function(){
+    console.log(_skills);
+    if(pgsection.hasChildNodes() == false){
+        for(var skill of _skills){
+            if(skill.category.includes("programming")){
+    
+                var div = document.createElement('div');
+    
+                div.className = 'skillbar';
+              
+                div.innerHTML = `
+                <div class="skillbar-title"><span>${skill.title}</span></div>
+                <div class="skillbar-bar" style="width:${String(Math.min(100, Math.max((skill.competency / 5.0) * 100.0, 0)))}%;"></div>
+                <div class="skill-bar-percent">${skill.competency + '/5'}</div>
+                `;
+                pgsection.appendChild(div);
+            }
+        }
+    }
+});
+
---
+++ pages/resumeData.js
@@ -0,0 +1,50 @@
+var _skills = [
+    {
+      title: 'C#',
+      competency: 4,
+      category: ['programming', 'all'],
+    },
+    {
+        title: 'VB Script',
+        competency: 2,
+        category: ['programming', 'All'],
+    },
+    {
+        title: 'C',
+        competency: 2,
+        category: ['programming', 'All'],
+    },
+    {
+        title: 'Html+CSS',
+        competency: 3,
+        category: ['programming', 'All'],
+    },
+    {
+        title: 'Javascript',
+        competency: 3,
+        category: ['programming', 'All'],
+    },
+    {
+        title: 'Python',
+        competency: 2,
+        category: ['programming', 'All'],
+    },
+    {
+        title: 'MATLAB',
+        competency: 2,
+        category: ['programming', 'All'],
+    }];
+
+var skillcolors = [
+        '#6968b3',
+        '#37b1f5',
+        '#40494e',
+        '#515dd4',
+        '#e47272',
+        '#cc7b94',
+        '#3896e2',
+        '#c3423f',
+        '#d75858',
+        '#747fff',
+        '#64cb7b',
+];
