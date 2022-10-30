1620ca07f0ed18adb06d68a38bf3e8d174fc9a1b
fenil00 <fenildesai00@gmail.com>
2022-10-30 08:19:39 +0100

    first skill bars added

--- pages/resume.js
+++ pages/resume.js
@@ -6,15 +6,15 @@ pgms.addEventListener('click', function(){
     if(pgsection.hasChildNodes() == false){
         for(var skill of _skills){
             if(skill.category.includes("programming")){
-    
+                
                 var div = document.createElement('div');
     
                 div.className = 'skillbar';
               
                 div.innerHTML = `
-                <div class="skillbar-title"><span>${skill.title}</span></div>
-                <div class="skillbar-bar" style="width:${String(Math.min(100, Math.max((skill.competency / 5.0) * 100.0, 0)))}%;"></div>
-                <div class="skill-bar-percent">${skill.competency + '/5'}</div>
+                <div class="skillbar-title" style="background:${skillcolors[0]};"><span>${skill.title}</span></div>
+                <div class="skillbar-bar" style="width:${String(Math.min(100, Math.max((skill.competency / 5.0) * 100.0, 0)))}%;background:${skillcolors[0]};"></div>
+                <div class="skill-bar-percent" style="background:${skillcolors[0]};">${skill.competency + '/5'}</div>
                 `;
                 pgsection.appendChild(div);
             }
