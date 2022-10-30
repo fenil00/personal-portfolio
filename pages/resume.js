7c23002b3495c2d451718adb186f3f96f000c005
fenil00 <fenildesai00@gmail.com>
2022-10-30 10:18:36 +0100

    skills added

--- pages/resume.js
+++ pages/resume.js
@@ -1,8 +1,31 @@
+var pillsall = document.getElementById("pills-all-tab");
+var allsection = document.getElementById("all");
+
+/*pillsall.addEventListener('load', function(){*/
+$(function() {
+    if(allsection.hasChildNodes() == false){
+        for(var skill of _skills){
+            if(skill.category.includes("all")){
+                
+                var div = document.createElement('div');
+    
+                div.className = 'skillbar';
+                var cat = skillcolors.find(x => x.category == skill.category[0]);
+                div.innerHTML = `
+                <div class="skillbar-title" style="background:${cat.color};"><span>${skill.title}</span></div>
+                <div class="skillbar-bar" style="width:${String(Math.min(100, Math.max((skill.competency / 5.0) * 100.0, 0)))}%;background:${cat.color};"></div>
+                <div class="skill-bar-percent">${skill.competency + '/5'}</div>
+                `;
+                allsection.appendChild(div);
+            }
+        }
+    }
+});
+
 var pgms = document.getElementById("pills-programming-tab");
 var pgsection = document.getElementById("programming");
 
 pgms.addEventListener('click', function(){
-    console.log(_skills);
     if(pgsection.hasChildNodes() == false){
         for(var skill of _skills){
             if(skill.category.includes("programming")){
@@ -11,10 +34,12 @@ pgms.addEventListener('click', function(){
     
                 div.className = 'skillbar';
               
+                var cat = skillcolors.find(x => x.category == "programming");
+
                 div.innerHTML = `
-                <div class="skillbar-title" style="background:${skillcolors[0]};"><span>${skill.title}</span></div>
-                <div class="skillbar-bar" style="width:${String(Math.min(100, Math.max((skill.competency / 5.0) * 100.0, 0)))}%;background:${skillcolors[0]};"></div>
-                <div class="skill-bar-percent" style="background:${skillcolors[0]};">${skill.competency + '/5'}</div>
+                <div class="skillbar-title" style="background:${cat.color};"><span>${skill.title}</span></div>
+                <div class="skillbar-bar" style="width:${String(Math.min(100, Math.max((skill.competency / 5.0) * 100.0, 0)))}%;background:${cat.color};"></div>
+                <div class="skill-bar-percent">${skill.competency + '/5'}</div>
                 `;
                 pgsection.appendChild(div);
             }
@@ -22,3 +47,104 @@ pgms.addEventListener('click', function(){
     }
 });
 
+
+
+var pillslanguage = document.getElementById("pills-languages-tab");
+var languagesection = document.getElementById("languages");
+
+pillslanguage.addEventListener('click', function(){
+    if(languagesection.hasChildNodes() == false){
+        for(var skill of _skills){
+            if(skill.category.includes("languages")){
+                
+                var div = document.createElement('div');
+    
+                div.className = 'skillbar';
+              
+                var cat = skillcolors.find(x => x.category == "languages");
+
+                div.innerHTML = `
+                <div class="skillbar-title" style="background:${cat.color};"><span>${skill.title}</span></div>
+                <div class="skillbar-bar" style="width:${String(Math.min(100, Math.max((skill.competency / 5.0) * 100.0, 0)))}%;background:${cat.color};"></div>
+                <div class="skill-bar-percent">${skill.competency + '/5'}</div>
+                `;
+                languagesection.appendChild(div);
+            }
+        }
+    }
+});
+
+var pillswebdevelopment = document.getElementById("pills-webdevelopment-tab");
+var webdevelopmentsection = document.getElementById("webdevelopment");
+
+pillswebdevelopment.addEventListener('click', function(){
+    if(webdevelopmentsection.hasChildNodes() == false){
+        for(var skill of _skills){
+            if(skill.category.includes("webdevelopment")){
+                
+                var div = document.createElement('div');
+    
+                div.className = 'skillbar';
+              
+                var cat = skillcolors.find(x => x.category == "webdevelopment");
+
+                div.innerHTML = `
+                <div class="skillbar-title" style="background:${cat.color};"><span>${skill.title}</span></div>
+                <div class="skillbar-bar" style="width:${String(Math.min(100, Math.max((skill.competency / 5.0) * 100.0, 0)))}%;background:${cat.color};"></div>
+                <div class="skill-bar-percent">${skill.competency + '/5'}</div>
+                `;
+                webdevelopmentsection.appendChild(div);
+            }
+        }
+    }
+});
+
+var pillstools = document.getElementById("pills-tools-tab");
+var toolssection = document.getElementById("tools");
+
+pillstools.addEventListener('click', function(){
+    if(toolssection.hasChildNodes() == false){
+        for(var skill of _skills){
+            if(skill.category.includes("tools")){
+                
+                var div = document.createElement('div');
+    
+                div.className = 'skillbar';
+              
+                var cat = skillcolors.find(x => x.category == "tools");
+
+                div.innerHTML = `
+                <div class="skillbar-title" style="background:${cat.color};"><span>${skill.title}</span></div>
+                <div class="skillbar-bar" style="width:${String(Math.min(100, Math.max((skill.competency / 5.0) * 100.0, 0)))}%;background:${cat.color};"></div>
+                <div class="skill-bar-percent">${skill.competency + '/5'}</div>
+                `;
+                toolssection.appendChild(div);
+            }
+        }
+    }
+});
+
+var pillsdatabases = document.getElementById("pills-databases-tab");
+var databasessection = document.getElementById("databases");
+
+pillsdatabases.addEventListener('click', function(){
+    if(databasessection.hasChildNodes() == false){
+        for(var skill of _skills){
+            if(skill.category.includes("databases")){
+                
+                var div = document.createElement('div');
+    
+                div.className = 'skillbar';
+              
+                var cat = skillcolors.find(x => x.category == "databases");
+
+                div.innerHTML = `
+                <div class="skillbar-title" style="background:${cat.color};"><span>${skill.title}</span></div>
+                <div class="skillbar-bar" style="width:${String(Math.min(100, Math.max((skill.competency / 5.0) * 100.0, 0)))}%;background:${cat.color};"></div>
+                <div class="skill-bar-percent">${skill.competency + '/5'}</div>
+                `;
+                databasessection.appendChild(div);
+            }
+        }
+    }
+});
--- pages/resumeData.js
+++ pages/resumeData.js
@@ -7,44 +7,149 @@ var _skills = [
     {
         title: 'VB Script',
         competency: 2,
-        category: ['programming', 'All'],
+        category: ['programming', 'all'],
     },
     {
         title: 'C',
         competency: 2,
-        category: ['programming', 'All'],
+        category: ['programming', 'all'],
     },
     {
         title: 'Html+CSS',
         competency: 3,
-        category: ['programming', 'All'],
+        category: ['programming', 'webdevelopment','all'],
     },
     {
         title: 'Javascript',
         competency: 3,
-        category: ['programming', 'All'],
+        category: ['programming', 'webdevelopment', 'all'],
     },
     {
         title: 'Python',
         competency: 2,
-        category: ['programming', 'All'],
+        category: ['programming', 'all'],
     },
     {
         title: 'MATLAB',
         competency: 2,
         category: ['programming', 'All'],
+    },
+    {
+        title: 'German',
+        competency: 3,
+        category: ['languages', 'all'],
+    }, 
+    {
+        title: 'English',
+        competency: 4,
+        category: ['languages', 'all'],
+    },
+    {
+        title: 'Hindi',
+        competency: 5,
+        category: ['languages', 'all'],
+    },
+    {
+        title: 'Gujarati',
+        competency: 5,
+        category: ['languages', 'all'],
+    },
+    {
+        title: 'Git',
+        competency: 3,
+        category: ['tools','all'],
+    },
+    {
+        title: 'Postman',
+        competency: 3,
+        category: ['tools','all'],
+    },
+    {
+        title: 'VMWare',
+        competency: 2,
+        category: ['tools','all'],
+    },
+    {
+        title: 'Siemens TIA Portal + Beckhoff TwinCAT 3',
+        competency: 2,
+        category: ['tools','all'],
+    },
+    {
+        title: 'Visual Studio + VS Code + GitLab',
+        competency: 3,
+        category: ['tools','webdevelopment','all'],
+    },
+    {
+        title: 'ZSH',
+        competency: 2,
+        category: ['tools','webdevelopment','all'],
+    },
+    {
+        title: 'Git',
+        competency: 3,
+        category: ['tools','webdevelopment','all'],
+    },
+    {
+        title: 'Postman',
+        competency: 3,
+        category: ['tools','webdevelopment','all'],
+    },
+    {
+        title: 'Node.js',
+        competency: 3,
+        category: ['webdevelopment', 'all'],
+    },
+    {
+        title: 'Express',
+        competency: 3,
+        category: ['webdevelopment', 'all'],
+    },
+    {
+        title: 'SQL Server',
+        competency: 3,
+        category: ['databases', 'all'],
+    },
+    {
+        title: 'LiteDB',
+        competency: 3,
+        category: ['databases', 'all'],
+    },
+    {
+        title: 'MongoDB',
+        competency: 2,
+        category: ['databases','webdevelopment', 'all'],
     }];
-
+    
 var skillcolors = [
+{
+    category:"programming",
+    color: '#747fff'
+} ,
+{
+    category:"languages",
+    color: '#64cb7b'
+},
+{
+    category:"webdevelopment",
+    color : '#37b1f5'
+},
+{
+    category:"tools",
+    color : '#c3423f'
+},
+{
+    category:"databases",
+    color : '#cc7b94'
+}];  
+       
+      
+        /* '#40494e',
         '#6968b3',
         '#37b1f5',
-        '#40494e',
         '#515dd4',
         '#e47272',
         '#cc7b94',
         '#3896e2',
         '#c3423f',
-        '#d75858',
-        '#747fff',
-        '#64cb7b',
-];
+        '#d75858' */
+
